a6a7d6ce10773ba6a41eb02f452927656f0782d9
Diego Cristóbal Herreros <diecristher@gmail.com>
2021-12-30 13:23:29 +0100

    Feat: add printmessage function and define get and post methods

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/dcristobalh/api-rest-go/src"
 	"github.com/gorilla/mux"
 )
 
@@ -28,19 +30,79 @@ func main() {
 
 	// Route handles & endpoints
 
-	// Get all movies
+	// Get all clothes
 	router.HandleFunc("/clothes/", GetClothes).Methods("GET")
 
-	// Create a movie
+	// Create a clothes
 	router.HandleFunc("/clothes/", CreateClothes).Methods("POST")
 
-	// Delete a specific movie by the movieID
+	// Delete a specific clothes by the clothesID
 	router.HandleFunc("/clothes/{clothesid}", DeleteClothes).Methods("DELETE")
 
-	// Delete all movies
+	// Delete all clothes
 	router.HandleFunc("/clothes/", DeleteClothes).Methods("DELETE")
 
 	// serve the app
 	fmt.Println("Server at 8080")
 	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func GetClothes(w http.ResponseWriter, r *http.Request) {
+	db := src.SetupDB()
+
+	src.PrintMessage("Getting movies...")
+
+	// Get all movies from movies table that don't have movieID = "1"
+	rows, err := db.Query("SELECT * FROM clothes")
+
+	// check errors
+	src.CheckErr(err)
+
+	// var response []JsonResponse
+	var clothes []Clothes
+
+	// Foreach movie
+	for rows.Next() {
+		var id int
+		var clothesID string
+		var clothesName string
+
+		err = rows.Scan(&id, &clothesID, &clothesName)
+
+		// check errors
+		src.CheckErr(err)
+
+		clothes = append(clothes, Clothes{ClothesID: clothesID, ClothesName: clothesName})
+	}
+
+	var response = JsonResponse{Type: "success", Data: clothes}
+
+	json.NewEncoder(w).Encode(response)
+}
+
+func CreateClothes(w http.ResponseWriter, r *http.Request) {
+	clothesID := r.FormValue("clothesid")
+	clothesName := r.FormValue("clothesname")
+
+	var response = JsonResponse{}
+
+	if clothesID == "" || clothesName == "" {
+		response = JsonResponse{Type: "error", Message: "You are missing clothesID or clothesName parameter."}
+	} else {
+		db := src.SetupDB()
+
+		src.PrintMessage("Inserting clothes into DB")
+
+		fmt.Println("Inserting new clothes with ID: " + clothesID + " and name: " + clothesName)
+
+		var lastInsertID int
+		err := db.QueryRow("INSERT INTO clothes(clothesID, clothesName) VALUES($1, $2) returning id;", clothesID, clothesName).Scan(&lastInsertID)
+
+		// check errors
+		src.CheckErr(err)
+
+		response = JsonResponse{Type: "success", Message: "The movie has been inserted successfully!"}
+	}
+
+	json.NewEncoder(w).Encode(response)
 }
