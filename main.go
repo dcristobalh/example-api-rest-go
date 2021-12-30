d8a8a6e35be2e8bc599dcc5ba0cc0070c6bad811
Diego Cristóbal Herreros <diecristher@gmail.com>
2021-12-30 11:34:43 +0100

    Feat: add json structs to define and get data

--- main.go
+++ main.go
@@ -7,11 +7,24 @@ import (
 	"github.com/dcristobalh/api-rest-go/src"
 )
 
+// Set variables to connection database
 const (
 	DB_USER     = "postgres"
 	DB_PASSWORD = "mysecretpassword"
 	DB_NAME     = "posgres"
 )
+
+// Create structs to define and get data from database
+type Clothes struct {
+	ClothesID   string `json:"clothesid"`
+	ClothesName string `json:"clothesname"`
+}
+
+type JsonResponse struct {
+	Type    string    `json:"type"`
+	Data    []Clothes `json:"data"`
+	Message string    `json:"message"`
+}
 
 // Connect to the database
 func setupDB() *sql.DB {
