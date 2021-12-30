239deb584b13156a1b04236731a3283a394c1d7d
Diego Cristóbal Herreros <diecristher@gmail.com>
2021-12-30 11:21:17 +0100

    Fix: set src as package from src path

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 
-	chkerrors "github.com/dcristobalh/api-rest-go/src"
+	"github.com/dcristobalh/api-rest-go/src"
 )
 
 const (
@@ -18,7 +18,7 @@ func setupDB() *sql.DB {
 	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
 	db, err := sql.Open("postgres", dbinfo)
 
-	chkerrors.CheckErr(err)
+	src.CheckErr(err)
 
 	return DB
 }
